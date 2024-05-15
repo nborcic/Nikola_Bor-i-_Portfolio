fd2073ea35de1d9b8780f694b291538b17f59cbf
John Doe <nickborcic69@gmail.com>
2024-05-15 17:14:46 +0200

    text and images added to the project, missing some tune up on the images

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,24 +1,4 @@
 $(document).ready(function () {
-  var owl = $('.owl-carousel').owlCarousel({
-    items: 2, // Adjust this according to your needs
-    loop: true,
-    nav: true, // Disable default navigation buttons
-    dots: false, // Enable dots if needed
-    autoplay: true,
-    autoplayTimeout: 3000, // Adjust the autoplay timeout
-    autoplayHoverPause: true
-  });
-
-  $('.owl-next').click(function () {
-    owl.trigger('next.owl.carousel');
-  });
-
-  $('.owl-prev').click(function () {
-    owl.trigger('prev.owl.carousel');
-  });
-});
-
-$(document).ready(function () {
   var owl = $('.owl-carousel');
   owl.owlCarousel({
     items: 1,
