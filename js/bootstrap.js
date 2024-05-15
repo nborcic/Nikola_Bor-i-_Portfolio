dc036523b3edce267e8b14b890757a949d20a8d9
John Doe <nickborcic69@gmail.com>
2024-05-16 01:05:26 +0200

    small changes to the website, added a new project and updated the about page

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,18 +1,14 @@
-$(document).ready(function () {
-  var owl = $('.owl-carousel');
-  owl.owlCarousel({
-    items: 1,
-    loop: true,
-    nav: false,
-    autoplay: true,
-    autoplayTimeout: 3000,
-    autoplayHoverPause: true
-  });
-  $('.owl-next').click(function () {
-    owl.trigger('next.owl.carousel');
-  });
-  $('.owl-prev').click(function () {
-    owl.trigger('prev.owl.carousel', [300]);
+document.querySelectorAll('.btnn').forEach(input => {
+  input.addEventListener('click', function () {
+    const textToCopy = this.value;
+    navigator.clipboard.writeText(textToCopy).then(() => {
+      console.log(textToCopy + ' copied to clipboard');
+    }).catch(error => {
+      console.error('Could not copy text: ', error);
+    });
   });
 });
 
+
+//make baloon for copy to clipboard info
+//add same to contacts page
