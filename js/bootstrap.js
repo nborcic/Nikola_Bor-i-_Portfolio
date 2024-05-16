2e04728153c65aba65dcc5ffad2926d640fb016e
John Doe <nickborcic69@gmail.com>
2024-05-16 13:50:30 +0200

    stuff geeting close to finish

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,14 +1,43 @@
-document.querySelectorAll('.btnn').forEach(input => {
-  input.addEventListener('click', function () {
-    const textToCopy = this.value;
-    navigator.clipboard.writeText(textToCopy).then(() => {
-      console.log(textToCopy + ' copied to clipboard');
-    }).catch(error => {
-      console.error('Could not copy text: ', error);
-    });
+document.addEventListener('DOMContentLoaded', (event) => {
+  const myName = "Nikola Borčić";
+  const myEmail = "nickborcic1@ymail.com";
+  const myPhone = "486 44 338";
+
+  function showCopiedText(copyInput, id) {
+    if (copyInput.value === myName && id === 1) {
+      copyInput.value = 'Copied';
+      setTimeout(() => {
+        copyInput.value = myName;
+        console.log(copyInput.value);
+      }, 2000);
+    } else if (copyInput.value === myEmail && id === 2) {
+      copyInput.value = 'Copied';
+      setTimeout(() => {
+        copyInput.value = myEmail;
+        console.log(copyInput.value);
+      }, 2000);
+    } else if (copyInput.value === myPhone && id === 3) {
+      copyInput.value = 'Copied';
+      setTimeout(() => {
+        copyInput.value = myPhone;
+        console.log(copyInput.value);
+      }, 2000);
+    }
+  }
+
+  const inputName = document.getElementById('inputName');
+  const inputEmail = document.getElementById('inputEmail');
+  const inputPhone = document.getElementById('inputPhone');
+
+  inputName.addEventListener('click', function () {
+    showCopiedText(inputName, 1);
+  });
+
+  inputEmail.addEventListener('click', function () {
+    showCopiedText(inputEmail, 2);
+  });
+
+  inputPhone.addEventListener('click', function () {
+    showCopiedText(inputPhone, 3);
   });
 });
-
-
-//make baloon for copy to clipboard info
-//add same to contacts page
